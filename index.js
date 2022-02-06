5fbd74b1fd9ae90c707f67f1885f839eb25b8fe3
Tanmayee Choudhury <66910404+tanmayee-07@users.noreply.github.com>
2022-02-06 21:03:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,23 +12,20 @@
 
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
-  
-
 
 // Initialize variables
 const auth = firebase.auth()
 const database = firebase.database()
 
-// Set up our register function
 function register () {
-  // Get all our input fields
+  // Get all the input fields
   email = document.getElementById('email').value
   password = document.getElementById('password').value
   full_name = document.getElementById('full_name').value
   locate = document.getElementById('location').value
  
 
-  // Validate input fields
+  // Validate the input fields
   if (validate_email(email) == false || validate_password(password) == false) {
     alert('Email or Password is Invalid!!')
     return
@@ -38,17 +35,12 @@ function register () {
     alert('One or More Extra Fields is Invalid!!')
     return
   }
- 
-  // Move on with Auth
+
   auth.createUserWithEmailAndPassword(email, password)
   .then(function() {
-    // Declare user variable
+    
     var user = auth.currentUser
-
-    // Add this user to Firebase Database
     var database_ref = database.ref()
-
-    // Create User data
     var user_data = {
       email : email,
       full_name : full_name,
@@ -56,14 +48,12 @@ function register () {
       last_login : Date.now()
     }
 
-    // Push to Firebase Database
     database_ref.child('users/' + user.uid).set(user_data)
 
-    // DOne
     alert('User Created!!')
   })
   .catch(function(error) {
-    // Firebase will use this to alert of its errors
+  
     var error_code = error.code
     var error_message = error.message
 
@@ -71,13 +61,11 @@ function register () {
   })
 }
 
-// Set up our login function
 function login () {
-  // Get all our input fields
+  
   email = document.getElementById('email').value
   password = document.getElementById('password').value
 
-  // Validate input fields
   if (validate_email(email) == false || validate_password(password) == false) {
     alert('Email or Password is Invalid!!')
     return
@@ -86,33 +74,25 @@ function login () {
 
   auth.signInWithEmailAndPassword(email, password)
   .then(function() {
-    // Declare user variable
+ 
     var user = auth.currentUser
-
-    // Add this user to Firebase Database
     var database_ref = database.ref()
-
-    // Create User data
     var user_data = {
       last_login : Date.now()
     }
 
-    // Push to Firebase Database
     database_ref.child('users/' + user.uid).update(user_data)
 
     alert('Yayy! User Logged In!!')
 
   })
   .catch(function(error) {
-    // Firebase will use this to alert of its errors
     var error_code = error.code
     var error_message = error.message
 
     alert(error_message)
   })
 }
-
-
 
 
 // Validate Functions
@@ -128,7 +108,6 @@ function validate_email(email) {
 }
 
 function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
   if (password < 6) {
     return false
   } else {
